22b24f8aa078a19d6ccdfbd38b1569a6cb6c4d5c
Nick <n1cv3nt@gmail.com>
2024-11-06 05:21:26 -0300

    Update App & Routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import Sidebar from './components/sidebar/sidebar';
-import 'leaflet/dist/leaflet.css';
-import Map from './components/map/map';
-import './app.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/home/home';
+import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
+import ProtectedRoute from './components/ProtectedRoute';
+import PublicRoute from './components/PublicRoute';
 
-const app = () => {
+const App = () => {
   return (
-    <div className="app">
-      <Sidebar/>
-      <div className='mapcontainer'>
-        <Map/>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="*"
+          element={<Navigate to="/home" />}
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
-};
+}
 
-export default app;
+export default App;
